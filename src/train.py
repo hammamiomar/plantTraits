777174b14a6a72abdf25d1022aed6dac7ffc227a
hammamiomar <oh524@nyu.edu>
2024-03-27 21:10:27 -0400

    updated scaler and dataloader, added lr scheduler, updated checkpoints system

--- src/train.py
+++ src/train.py
@@ -1,5 +1,6 @@
 # train.py
 import os
+import datetime
 import numpy as np
 import torch
 import torch.nn as nn
@@ -12,10 +13,10 @@ from src.model import PlantModel
 from src.utils import calculate_normalization_stats
 from src.utils import init_weights
 
-def train(csv_file,image_dir,batch_size=32,num_epochs=10,num_workers=4,early_stopping_patience=10):
+def train(X_train,y_train,batch_size=32,num_epochs=10,num_workers=4,early_stopping_patience=10,device="cuda"):
     # Create an instance of the plantDataset (without transforms)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    #device="mps"
+    #device = torch.device("cuda" if torch.cuda.is_available()else "cpu")
+    #ddevice="mps"
     print("Using device:", device)
 
     transform = transforms.Compose([
@@ -25,23 +26,22 @@ def train(csv_file,image_dir,batch_size=32,num_epochs=10,num_workers=4,early_sto
     ])
 
     # Create a new instance of the plantDataset with the transformation pipeline
-    dataset = plantDataset(csv_file=csv_file, image_dir=image_dir, transform=transform)
+    dataset = plantDataset(X_train=X_train, y_train=y_train, transform=transform)
 
-    target_mean = dataset.data[dataset.target_cols].mean().values
-    target_std = dataset.data[dataset.target_cols].std().values
-    # Create a DataLoader
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
     # Create an instance of the model
     
-    model = PlantModel(num_ancillary_features=len(dataset.input_cols), num_output_features=len(dataset.target_cols))
+    model = PlantModel(num_ancillary_features=len(dataset.input_cols), num_output_features=dataset.y_train.shape[1])
     
     #model.apply(init_weights)
     model.to(device)
 
     # Define loss function and optimizer
     criterion = nn.MSELoss()  # Mean Squared Error loss for regression
-    optimizer = optim.Adam(model.parameters(), lr=0.0001)  # Adam optimizer with learning rate 0.001
+    optimizer = optim.Adam(model.parameters(), lr=0.001)  # Adam optimizer with learning rate 0.001
+
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.01, steps_per_epoch=len(dataloader), epochs=num_epochs)
 
 
         # Create a directory to save the model weights
@@ -77,28 +77,48 @@ def train(csv_file,image_dir,batch_size=32,num_epochs=10,num_workers=4,early_sto
             # Backward pass and optimization
             loss.backward()
             optimizer.step()
+            scheduler.step()
             
             running_loss += loss.item() #* images.size(0)
 
             progress_bar.set_postfix(loss=loss.item())
-        
+            
         epoch_loss = running_loss / len(dataloader.dataset)
         print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {epoch_loss:.4f}")
         
-        # Early stopping
+        #Early stopping
         if epoch_loss < best_loss:
             best_loss = epoch_loss
             counter = 0
-            # Save the best model weights
-            torch.save(model.state_dict(), os.path.join(checkpoint_dir, 'best_model.pth'))
-            # Save the target normalization statistics
-            np.save(os.path.join(checkpoint_dir, 'target_mean.npy'), target_mean)
-            np.save(os.path.join(checkpoint_dir, 'target_std.npy'), target_std)
+            timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+            best_model_path = os.path.join(checkpoint_dir, f'best_model_{timestamp}.pth')
+            torch.save(model.state_dict(), best_model_path)
+            print(f"Best model saved at '{best_model_path}'")
         else:
             counter += 1
             if counter >= early_stopping_patience:
                 print(f"Early stopping at epoch {epoch+1}")
+                timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+                early_stop_model_path = os.path.join(checkpoint_dir, f'early_stop_model_{timestamp}.pth')
+                torch.save(model.state_dict(), early_stop_model_path)
+                print(f"Early stop model saved at '{early_stop_model_path}'")
                 break
+
+        # Save a checkpoint every 5 epochs with timestamp
+        if (epoch + 1) % 5 == 0:
+            timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+            checkpoint_path = os.path.join(checkpoint_dir, f'checkpoint_epoch_{epoch+1}_{timestamp}.pth')
+            torch.save(model.state_dict(), checkpoint_path)
+            print(f"Checkpoint saved at '{checkpoint_path}'")
+
     print('Finished Training')
-    # Save the final model weights
-    torch.save(model.state_dict(), os.path.join(checkpoint_dir, 'final_model.pth'))
+
+    # Save the final model with a timestamp
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    final_model_path = os.path.join(checkpoint_dir, f'final_model_{timestamp}.pth')
+    torch.save(model.state_dict(), final_model_path)
+    print(f"Final model saved at '{final_model_path}'")
+    
+    
+#if __name__ == "__main__":
+ #   train(csv_file="data/train.csv",image_dir="data/images",batch_size=32,num_epochs=20,num_workers=4,early_stopping_patience=10,device="mps")
