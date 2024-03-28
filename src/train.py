ec6bfa4f13a2ccb0dd2d4d3184fd95b54510cfba
hammamiomar <oh524@nyu.edu>
2024-03-28 00:26:34 -0400

    added checkpoint resume. remove scheduler for now

--- src/train.py
+++ src/train.py
@@ -1,6 +1,7 @@
 # train.py
 import os
 import datetime
+import re
 import numpy as np
 import torch
 import torch.nn as nn
@@ -13,7 +14,28 @@ from src.model import PlantModel
 from src.utils import calculate_normalization_stats
 from src.utils import init_weights
 
-def train(X_train,y_train,batch_size=32,num_epochs=10,num_workers=4,early_stopping_patience=10,device="cuda"):
+def find_latest_checkpoint(checkpoint_dir):
+    # Define the regex pattern for matching filenames and extracting epoch numbers
+    pattern = re.compile(r"checkpoint_epoch_(\d+).pth")
+
+    # Initialize variables to keep track of the highest epoch and corresponding file
+    highest_epoch = -1
+    latest_checkpoint_file = None
+
+    # Iterate over all files in the checkpoint directory
+    for filename in os.listdir(checkpoint_dir):
+        match = pattern.match(filename)
+        if match:
+            # Extract the epoch number from the filename
+            epoch_num = int(match.group(1))
+            if epoch_num > highest_epoch:
+                highest_epoch = epoch_num
+                latest_checkpoint_file = filename
+
+    return latest_checkpoint_file
+
+def train(X_train,y_train,batch_size=32,num_epochs=10,num_workers=4,early_stopping_patience=50,device="cuda"):
+    torch.manual_seed(420)
     # Create an instance of the plantDataset (without transforms)
     #device = torch.device("cuda" if torch.cuda.is_available()else "cpu")
     #ddevice="mps"
@@ -38,21 +60,31 @@ def train(X_train,y_train,batch_size=32,num_epochs=10,num_workers=4,early_stoppi
     model.to(device)
 
     # Define loss function and optimizer
-    criterion = nn.MSELoss()  # Mean Squared Error loss for regression
+    criterion = nn.MSELoss()  # Mean Average Error loss for regression
     optimizer = optim.Adam(model.parameters(), lr=0.001)  # Adam optimizer with learning rate 0.001
 
-    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.01, steps_per_epoch=len(dataloader), epochs=num_epochs)
+    #scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.01, steps_per_epoch=len(dataloader), epochs=num_epochs)
 
-
+    start_epoch = 0
+    best_loss = float('inf')  # Initialize the best loss to infinity
+    #counter = 0
+    
         # Create a directory to save the model weights
     checkpoint_dir = 'checkpoints'
     os.makedirs(checkpoint_dir, exist_ok=True)
-
-    best_loss = float('inf')  # Initialize the best loss to infinity
-    counter = 0
-    checkpoint_dir = 'checkpoints'
-    os.makedirs(checkpoint_dir, exist_ok=True)
-    for epoch in range(num_epochs):
+    
+    latestCheckpoint=find_latest_checkpoint(checkpoint_dir)
+    if latestCheckpoint:
+        checkpoint_path = os.path.join(checkpoint_dir, latestCheckpoint)
+        checkpoint = torch.load(checkpoint_path,map_location=device)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        start_epoch = checkpoint['epoch']
+        best_loss = checkpoint['best_loss']
+        print(f"Loaded checkpoint from '{checkpoint_path}' at epoch {start_epoch}")
+    
+    
+    for epoch in range(start_epoch,num_epochs):
         model.train()
         running_loss = 0.0
         progress_bar = tqdm(dataloader, desc=f"Epoch {epoch+1}/{num_epochs}", unit="batch")
@@ -77,7 +109,7 @@ def train(X_train,y_train,batch_size=32,num_epochs=10,num_workers=4,early_stoppi
             # Backward pass and optimization
             loss.backward()
             optimizer.step()
-            scheduler.step()
+            #scheduler.step()
             
             running_loss += loss.item() * images.size(0)
 
@@ -87,28 +119,37 @@ def train(X_train,y_train,batch_size=32,num_epochs=10,num_workers=4,early_stoppi
         print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {epoch_loss:.4f}")
         
         #Early stopping
-        # if epoch_loss < best_loss:
-        #     best_loss = epoch_loss
-        #     counter = 0
-        #     timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-        #     best_model_path = os.path.join(checkpoint_dir, f'best_model_{timestamp}.pth')
-        #     torch.save(model.state_dict(), best_model_path)
-        #     print(f"Best model saved at '{best_model_path}'")
-        # else:
-        #     counter += 1
-        #     if counter >= early_stopping_patience:
-        #         print(f"Early stopping at epoch {epoch+1}")
-        #         timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-        #         early_stop_model_path = os.path.join(checkpoint_dir, f'early_stop_model_{timestamp}.pth')
-        #         torch.save(model.state_dict(), early_stop_model_path)
-        #         print(f"Early stop model saved at '{early_stop_model_path}'")
-        #         break
+        if epoch_loss < best_loss:
+            best_loss = epoch_loss
+            counter = 0
+            best_model_path = os.path.join(checkpoint_dir, f'best_model.pth')
+            torch.save({
+                'epoch':epoch+1,
+                'model_state_dict':model.state_dict(),
+                'optimizer_state_dict':optimizer.state_dict,
+                'best_loss':best_loss}, best_model_path)
+            print(f"Best model saved at '{best_model_path}'")
+        else:
+            counter += 1
+            if counter >= early_stopping_patience:
+                print(f"Early stopping at epoch {epoch+1}")
+                early_stop_model_path = os.path.join(checkpoint_dir, f'early_stop_model.pth')
+                torch.save({
+                'epoch':epoch+1,
+                'model_state_dict':model.state_dict(),
+                'optimizer_state_dict':optimizer.state_dict,
+                'best_loss':best_loss}, early_stop_model_path)
+                print(f"Early stop model saved at '{early_stop_model_path}'")
+                break
 
         # Save a checkpoint every 5 epochs with timestamp
         if (epoch + 1) % 5 == 0:
-            timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-            checkpoint_path = os.path.join(checkpoint_dir, f'checkpoint_epoch_{epoch+1}_{timestamp}.pth')
-            torch.save(model.state_dict(), checkpoint_path)
+            checkpoint_path = os.path.join(checkpoint_dir, f'checkpoint_epoch_{epoch+1}.pth')
+            torch.save({
+                'epoch':epoch+1,
+                'model_state_dict':model.state_dict(),
+                'optimizer_state_dict':optimizer.state_dict,
+                'best_loss':best_loss}, checkpoint_path)
             print(f"Checkpoint saved at '{checkpoint_path}'")
 
     print('Finished Training')
